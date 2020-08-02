0895fb0d2fffb55a9243601b01b7fe9c4b16d27b
wreckss <46939881+Wreckss@users.noreply.github.com>
2020-08-01 18:23:57 -0600

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by taco on 8/1/20.
+//  Copyright © 2020 Wrecks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
